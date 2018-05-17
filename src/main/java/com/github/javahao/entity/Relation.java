f80cad6db6dfc061fefac9463eccda0eb3ea72dc
haolianshuai <353475081@qq.com>
2018-05-17 19:39:24 +0800

    升级关系表定义变量名称以及备注信息

--- src/main/java/com/github/javahao/entity/Relation.java
+++ src/main/java/com/github/javahao/entity/Relation.java
@@ -10,6 +10,8 @@ public class Relation {
     private String target;
     private RelationEnum type;
     private String foreignKey;
+    private String var;
+    private String varDesc;
 
     public String getTarget() {
         return target;
@@ -35,16 +37,34 @@ public class Relation {
         this.foreignKey = foreignKey;
     }
 
+    public String getVar() {
+        return var;
+    }
+
+    public void setVar(String var) {
+        this.var = var;
+    }
+
+    public String getVarDesc() {
+        return varDesc;
+    }
+
+    public void setVarDesc(String varDesc) {
+        this.varDesc = varDesc;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Relation)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         Relation relation = (Relation) o;
 
         if (target != null ? !target.equals(relation.target) : relation.target != null) return false;
         if (type != relation.type) return false;
-        return foreignKey != null ? foreignKey.equals(relation.foreignKey) : relation.foreignKey == null;
+        if (foreignKey != null ? !foreignKey.equals(relation.foreignKey) : relation.foreignKey != null) return false;
+        if (var != null ? !var.equals(relation.var) : relation.var != null) return false;
+        return varDesc != null ? varDesc.equals(relation.varDesc) : relation.varDesc == null;
     }
 
     @Override
@@ -52,15 +72,8 @@ public class Relation {
         int result = target != null ? target.hashCode() : 0;
         result = 31 * result + (type != null ? type.hashCode() : 0);
         result = 31 * result + (foreignKey != null ? foreignKey.hashCode() : 0);
+        result = 31 * result + (var != null ? var.hashCode() : 0);
+        result = 31 * result + (varDesc != null ? varDesc.hashCode() : 0);
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Relation{" +
-                "target='" + target + '\'' +
-                ", type=" + type +
-                ", foreignKey='" + foreignKey + '\'' +
-                '}';
     }
 }
